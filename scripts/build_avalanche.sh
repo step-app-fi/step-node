2ea92432686662ddf55ad3b51c60817c12c28c4a
Kirill Varlamov <kirill@ongrid.pro>
2022-07-21 17:07:14 +0400

    feat(subnet-evm): adapt Dockerfile and scripts for unified testnet+subnet image

--- scripts/build_avalanche.sh
+++ scripts/build_avalanche.sh
@@ -41,4 +41,4 @@ source "$AVALANCHE_PATH"/scripts/versions.sh
 source "$AVALANCHE_PATH"/scripts/constants.sh
 
 echo "Building AvalancheGo..."
-go build -ldflags "-X github.com/ava-labs/avalanchego/version.GitCommit=$git_commit $static_ld_flags" -o "$avalanchego_path" "$AVALANCHE_PATH/main/"*.go
+go build -ldflags "$static_ld_flags" -o "$avalanchego_path" "$AVALANCHE_PATH/main/"*.go
