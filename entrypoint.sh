f27b41c3968901f5b091e54fdfdea4d8c6ca5df2
Vadim Volkov <v.volkov@ongrid.pro>
2023-01-30 17:28:14 +0400

    chore: graceful termination of container

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ cp /root/.avalanchego/configs/chains-relaxed/$BLOCKCHAIN_ID_ENV/config.json ${TM
 cp /root/.avalanchego/configs/chains-restricted/$BLOCKCHAIN_ID_ENV/config.json ${TMPFILE_RESTRICTED}
 cat ${TMPFILE_RELAXED} | envsubst > /root/.avalanchego/configs/chains-relaxed/$BLOCKCHAIN_ID_ENV/config.json
 cat ${TMPFILE_RESTRICTED} | envsubst > /root/.avalanchego/configs/chains-restricted/$BLOCKCHAIN_ID_ENV/config.json
-avalanchego $* 
+exec avalanchego $*
